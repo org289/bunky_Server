0aac4a15e2603691614181f4583435a5bf734a03
org28 <org289@gmail.com>
2020-03-23 22:17:30 +0200

    first commit - starter spring boot

---
+++ src/main/java/com/bunky/server/BunkyServerApplication.java
@@ -0,0 +1,13 @@
+package com.bunky.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunkyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunkyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bunky/server/BunkyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunky.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunkyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
