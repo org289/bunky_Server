a7d33ffeadbc093bdf524dbe4560e770a36dd075
org28 <org289@gmail.com>
2020-05-03 17:55:44 +0300

    added function test to see if front can send user object. added media type

--- src/main/java/com/bunky/server/Controller/LoginController.java
+++ src/main/java/com/bunky/server/Controller/LoginController.java
@@ -7,6 +7,7 @@ import com.bunky.server.Dao.LoginDao;
 import com.bunky.server.Entity.Apartment;
 import com.bunky.server.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,7 +49,7 @@ public class LoginController {
         return loginDao.getAllUsers();
     }
 
-    @RequestMapping(value = "/testSendingUser", method = RequestMethod.POST)
+    @RequestMapping(value = "/testSendingUser", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createUser(@RequestBody User newUser) {
         System.out.println(newUser);
     }
